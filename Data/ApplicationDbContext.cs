4a702634fbcbe384e71b114617cb1b85f63c542e
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-04-14 21:40:36 -0400

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+          
+        }
+    }
+}
